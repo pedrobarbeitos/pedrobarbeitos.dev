c4c45542ebe0593ca57df21c696dadeab08339ec
Pedro Barbeitos <barbeitos.pedro@gmail.com>
2023-10-07 14:14:21 +0100

    First commit

---
+++ src/app/components/Float.tsx
@@ -0,0 +1,85 @@
+
+'use client';
+import React, { useEffect, useRef } from 'react';
+import './Float.scss'
+
+type Props = {};
+
+const Float: React.FC<Props> = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const img = imgRef.current;
+
+    if (!canvas || !img) {
+      return;
+    }
+
+    const ctx = canvas.getContext('2d');
+
+    function mix(a: number, b: number, l: number) {
+      return a + (b - a) * l;
+    }
+
+    function upDown(v: number) {
+      return Math.sin(v) * 0.5 + 0.5;
+    }
+
+    function render(time: number) {
+      time *= 0.001;
+
+      resize(canvas);
+
+      var t1 = time;
+      var t2 = time * 0.8;
+
+      for (var dstY = 0; dstY < canvas.height; ++dstY) {
+        var v = dstY / canvas.height;
+        var off1 = Math.sin((v + 0.5) * mix(3, 12, upDown(t1))) * 100;
+        var off2 = Math.sin((v + 0.5) * mix(3, 12, upDown(t2))) * 100;
+        var off = off1 + off2;
+
+        var srcY = dstY * img.height / canvas.height + off;
+
+        srcY = Math.max(0, Math.min(img.height - 1, srcY));
+
+        ctx.drawImage(
+          img,
+          0, srcY, img.width, 1,
+          0, dstY, canvas.width, 1
+        );
+      }
+
+      requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+
+  }, []);
+
+  function resize(canvas: HTMLCanvasElement) {
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+
+    if (width !== canvas.width || height !== canvas.height) {
+      canvas.width = width;
+      canvas.height = height;
+    }
+  }
+
+  return (
+    <div>
+      <canvas className='canvas' ref={canvasRef}></canvas>
+      <img
+        className='image'
+        ref={imgRef}
+        src={'/profile.jpg'}
+        alt=''
+      />
+    </div>
+  );
+};
+
+export default Float;
